0f11d3214342b2bbcb2c6a9c44519789c7eba081
Giovanna Menegoli <giovanna.menegoli@hotmail.com>
2022-01-27 19:52:59 -0300

    Repositorio do curso de logica de programação

---
+++ ola_mundo/js/script.js
@@ -0,0 +1,7 @@
+// document.getElementById('saida').innerHTML = 'Olá Mundo';
+// alert ("Olá Mundo")
+
+let nome=prompt("Digite o seu Nome:");
+alert(`Olá ${nome}, seja bem vinde a imersão de Front End`);
+
+document.getElementById("saida").innerHTML = `Olá ${nome}, seja bem vinde a imersão de Front end`
